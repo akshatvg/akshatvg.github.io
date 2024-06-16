b225b30a6ba72d753cfaf17ed04b0451ba14eff2
Akshat Gupta <33231334+akshatvg@users.noreply.github.com>
2024-06-16 14:57:47 +0530

    content updates

--- src/Components/AboutSection/AboutSection.jsx
+++ src/Components/AboutSection/AboutSection.jsx
@@ -28,13 +28,21 @@ function AboutSection() {
                             About <span className={classes.textColored}>Me</span>
                         </Typography>
                         <Typography variant="h6" className={classes.subtitle}>
-                        Hailing from Chennai in India, I currently work at LegalForce RAPC Worldwide as an <b>Operations and Engineering Manager</b>. I lead the Engineering, Design, and Product teams for multiple ventures within the company, comprising more than 50 personnel that are operating from various locations across the globe.
+                            CEO of <a href="https://www.peritys.com">Peritys</a> and entrepreneur from Chennai, India, I specialize in leveraging IT and marketing consultancy to bring SaaS innovations like <a href="https://www.interacts.ai">Interacts AI</a> to market. My journey began with founding VAMSA Tech Private Limited during my university years, where I discovered my passion for converting groundbreaking ideas into reality. My experience includes a leadership role at LegalForce RAPC Worldwide, guiding global teams in engineering, design, and product management.
                         </Typography>
                         <Typography variant="h6" className={classes.subtitle}>
-                        I started my career as a <b>Software Developer</b> where I explored multiple technical domains till I found the right fit for me- <b>Web Development</b>. In my eagerness to keep exploring, I also learned about SEO, SEM, and <b>Digital Marketing</b> in my free time. While pursuing my undergraduate degree, I co-founded a service-based <b>startup</b> along with a few friends, which honed my business acumen and entrepreneurial instincts. I am passionate about my current role which is primarily focused on technical project management; the role occasionally also requires me to roll up my sleeves and showcase my technological prowess.
+                            What I bring to the table:
+                            <ul>
+                                <li><b>Strategic Leadership:</b> Proven ability in ideation, developing offshore development centers, and global expansion, transforming concepts into profitable business models.</li>
+                                <li><b>Project Mastery:</b> Expert in navigating the complete project lifecycle, ensuring products resonate with their market and deliver exceptional user experiences.</li>
+                                <li><b>Technical Acumen:</b> A foundation in software development and DevOps, committed to building scalable, efficient systems.</li>
+                                <li><b>Team Development:</b> A track record of assembling and nurturing teams, promoting innovation, and achieving collective excellence.</li>
+                                <li><b>Strategic IT Consultancy:</b> Aligning technology strategies with business goals to optimize growth and process efficiency.</li>
+                                <li><b>Financial Stewardship:</b> Skilled in navigating the financial and operational facets of business to fuel sustainable growth.</li>
+                            </ul>
                         </Typography>
                         <Typography variant="h6" className={classes.subtitle}>
-                            In my free time, I binge on Netflix and YouTube, listen to songs, go for long drives, and try new cuisines at local restaurants (foodie at heart).
+                            I am driven by the challenge of turning the nascent stages of ideas into thriving business entities, demonstrating a keen ability to scale ventures from zero to one across diverse global landscapes.
                         </Typography>
                     </Grid>
                 </Grid>
@@ -60,4 +68,3 @@ function AboutSection() {
 }
 
 export default AboutSection;
-
